c53d0236ed4338de1cc34ada915122f4981fcc4c
jsay <jsxhxx0819@gmail.com>
2023-07-07 16:32:50 +0900

    230707 수업 lightbox

---
+++ js/lightbox.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+  var pics = document.querySelectorAll(".pic");
+  var lightbox = document.querySelector("#lightbox");
+  var lightboxImage = document.querySelector("#lightboxImage");
+
+  for (var i = 0; i < pics.length; i++) {
+    pics[i].addEventListener("click", showlightBox);
+  }
+  function showlightBox() {
+    var bigLocation = this.getAttribute("data-src");
+    lightboxImage.setAttribute("src", bigLocation);
+    lightbox.style.display = "block";
+  }
+  lightbox.onclick = function () {
+    lightbox.style.display = "none";
+  };
+};
